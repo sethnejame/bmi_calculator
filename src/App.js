2a5ea47fc3fcacc35d9077277b1e1ab03c27881f
Seth NeJame <sethnejame@gmail.com>
2019-08-05 11:38:40 +0200

    Installing Cypress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DisplayResult from './components/DisplayResult';
-
+import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
