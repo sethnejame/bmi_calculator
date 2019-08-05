7ccd43c15645b4f41e3139554fa54268b6c8cb04
Seth NeJame <sethnejame@gmail.com>
2019-08-05 10:55:07 +0200

    Adding calculate function to displayresult

--- src/components/DisplayResult.js
+++ src/components/DisplayResult.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class DisplayResult extends Component {
+  calculate() {
+    var weight= this.props.weight;
+    var height= this.props.height;
+  }
 
   render() {
     return (
       <div id='response'>
+        {this.calculate()}
       </div>
     )
   }
