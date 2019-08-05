27331a6bdc7b5f94807ddb1007ac9967844222f0
Seth NeJame <sethnejame@gmail.com>
2019-08-05 10:40:58 +0200

    Adding form fields for height & weight

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label>Weight(kg)</label>
+          <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value })} />
+        </div>
+
+        <div>
+          <label>Height(cm)</label>
+          <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
+        </div>
+      </div>
+    );
+        }
 }
 
 export default App;
