79d16de95c9a39008e194e65e2e5e62e46edfc14
Seth NeJame <sethnejame@gmail.com>
2019-08-06 14:42:28 +0200

    Updates

---
+++ precache-manifest.c34069703a0a82eb8c617d0f8928a29b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a87d1774e63883a698c8addf0353df",
+    "url": "/bmi_calculator/index.html"
+  },
+  {
+    "revision": "2c22df2b3f2563cc7186",
+    "url": "/bmi_calculator/static/css/main.2d63b268.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/bmi_calculator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2c22df2b3f2563cc7186",
+    "url": "/bmi_calculator/static/js/main.124f3da5.chunk.js"
+  },
+  {
+    "revision": "46e1681b346856b4d934",
+    "url": "/bmi_calculator/static/js/runtime~main.53f89852.js"
+  }
+]);
